5b757fa49dad7c8a2456dc1c1e02f500250b72fa
Lokman-Hossain-Mujahid <rclash163@gmail.com>
2023-06-24 03:36:02 +0600

    About Page Responsive Done

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -41,9 +41,9 @@ const about = () => {
             </Head>
             <main className='flex w-full flex-col items-center justify-center dark:text-light'>
                 <Layout className='pt-16'>
-                    <AnimatedText text="Passion Ignites Purpose!" className="mb-16" />
-                    <div className='grid w-full grid-cols-8 gap-16'>
-                        <div className='col-span-3 flex flex-col items-start justify-start'>
+                    <AnimatedText text="Passion Ignites Purpose!" className="mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8" />
+                    <div className='grid w-full grid-cols-8 gap-16 sm:gap-8'>
+                        <div className='col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8'>
                             <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75'>Biography</h2>
                             <p className='font-medium my-4'> My name is Mujahid, and I have recently embarked on a journey towards achieving my professional aspirations by pursuing a Marketing Honors degree at a prestigious national university. Although my chosen field of study may not be directly aligned with information technology, I have always harbored a fervent passion for the technological realm. This enthusiasm prompted me to delve into the realm of web development, where I have cultivated my skills as a proficient MERN stack developer over the past year.
                             </p>
@@ -56,29 +56,30 @@ const about = () => {
 
                             <p className='font-medium my-4'>I eagerly embrace the challenges that lie ahead and am committed to making a substantial impact in the technological arena through my unwavering dedication, punctuality, and unrelenting pursuit of expertise as a web developer.</p>
                         </div>
-                        <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light'>
+
+                        <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light x;l:col-span-4 md:order-1 md:col-span-8'>
                             <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light' />
                             <Image src={profilePic} alt='Mujahid' className='w-full h-auto rounded-2xl' priority
                                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
                         </div>
-                        <div className='col-span-2 flex flex-col items-end justify-between'>
-                            <div className='flex flex-col items-end justify-center'>
-                                <span className='inline-block text-7xl font-bold'>
+                        <div className='col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center md:order-3'>
+                            <div className='flex flex-col items-end justify-center xl:items-center'>
+                                <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                                     <AnimatedNumbers value={1} />+
                                 </span>
-                                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>Years of experience</h2>
+                                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>Years of experience</h2>
                             </div>
-                            <div className='flex flex-col items-end justify-center'>
-                                <span className='inline-block text-7xl font-bold'>
+                            <div className='flex flex-col items-end justify-center xl:items-center'>
+                                <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                                     <AnimatedNumbers value={15} />+
                                 </span>
-                                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>Projects Completed</h2>
+                                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>Projects Completed</h2>
                             </div>
-                            <div className='flex flex-col items-end justify-center'>
-                                <span className='inline-block text-7xl font-bold'>
+                            <div className='flex flex-col items-end justify-center xl:items-center'>
+                                <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                                     <AnimatedNumbers value={10} />+
                                 </span>
-                                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>Skills Learned</h2>
+                                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>Skills Learned</h2>
                             </div>
                         </div>
                     </div>
