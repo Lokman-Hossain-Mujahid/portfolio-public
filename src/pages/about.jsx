ef5d683bc3cf66ba3186f68dd0c4a1001178b407
Lokman-Hossain-Mujahid <rclash163@gmail.com>
2023-06-23 20:18:23 +0600

    light and dark mode Done

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -39,12 +39,12 @@ const about = () => {
                 <title>LokmanMujahid | About Page</title>
                 <meta name="description" content='any description' />
             </Head>
-            <main className='flex w-full flex-col items-center justify-center'>
+            <main className='flex w-full flex-col items-center justify-center dark:text-light'>
                 <Layout className='pt-16'>
                     <AnimatedText text="Passion Ignites Purpose!" className="mb-16" />
                     <div className='grid w-full grid-cols-8 gap-16'>
                         <div className='col-span-3 flex flex-col items-start justify-start'>
-                            <h2 className='mb-4 text-lg font-bold uppercase text-dark/75'>Biography</h2>
+                            <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75'>Biography</h2>
                             <p className='font-medium my-4'> My name is Mujahid, and I have recently embarked on a journey towards achieving my professional aspirations by pursuing a Marketing Honors degree at a prestigious national university. Although my chosen field of study may not be directly aligned with information technology, I have always harbored a fervent passion for the technological realm. This enthusiasm prompted me to delve into the realm of web development, where I have cultivated my skills as a proficient MERN stack developer over the past year.
                             </p>
 
@@ -56,8 +56,8 @@ const about = () => {
 
                             <p className='font-medium my-4'>I eagerly embrace the challenges that lie ahead and am committed to making a substantial impact in the technological arena through my unwavering dedication, punctuality, and unrelenting pursuit of expertise as a web developer.</p>
                         </div>
-                        <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8'>
-                            <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark' />
+                        <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light'>
+                            <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light' />
                             <Image src={profilePic} alt='Mujahid' className='w-full h-auto rounded-2xl' />
                         </div>
                         <div className='col-span-2 flex flex-col items-end justify-between'>
@@ -65,19 +65,19 @@ const about = () => {
                                 <span className='inline-block text-7xl font-bold'>
                                     <AnimatedNumbers value={1} />+
                                 </span>
-                                <h2 className='text-xl font-medium capitalize text-dark/75'>Years of experience</h2>
+                                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>Years of experience</h2>
                             </div>
                             <div className='flex flex-col items-end justify-center'>
                                 <span className='inline-block text-7xl font-bold'>
                                     <AnimatedNumbers value={15} />+
                                 </span>
-                                <h2 className='text-xl font-medium capitalize text-dark/75'>Projects Completed</h2>
+                                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>Projects Completed</h2>
                             </div>
                             <div className='flex flex-col items-end justify-center'>
                                 <span className='inline-block text-7xl font-bold'>
                                     <AnimatedNumbers value={10} />+
                                 </span>
-                                <h2 className='text-xl font-medium capitalize text-dark/75'>Skills Learned</h2>
+                                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>Skills Learned</h2>
                             </div>
                         </div>
                     </div>
