96ef7c1951c27e924a4226c58cb88d1abc58da1e
Lokman-Hossain-Mujahid <rclash163@gmail.com>
2023-06-25 23:07:16 +0600

    resume updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
                 As a proficient full-stack developer, I am committed to bringing ideas to life through cutting-edge web applications. Delve into my recent projects and insightful articles, highlighting my mastery of React.js and expertise in the realm of web development.
               </p>
               <div className='flex items-center self-start mt-2 lg:self-center'>
-                <Link href="/resume.pdf" target={'_blank'} className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base' 
+                <Link href="./resume.pdf" target={'_blank'} className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base' 
                 download={true}
                 >Resume <LinkArrow className={"w-6 ml-1"} /> </Link>
                 <Link href="lokmanmujahid4@gmail.com" target={'_blank'} className='ml-4 text-lg font-medium capitalize text-dark underline dark:text-light md:text-base'>Contact</Link>
