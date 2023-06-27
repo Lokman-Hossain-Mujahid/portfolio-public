8cddb927d6605afeaa134b05c281b77638a4534e
Lokman-Hossain-Mujahid <rclash163@gmail.com>
2023-06-27 13:56:05 +0600

    contact.js code reversed

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -1,23 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { sendContactForm } from '@/lib/api';
-
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { name, email, subject, message } = req.body;
-    const formData = { name, email, subject, message };
-
-    try {
-      // Call your sendContactForm function or implement the email sending logic here
-      await sendContactForm(formData);
-
-      res.status(200).json({ message: 'Email sent successfully!' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Failed to send email.' });
-    }
-  } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
-  }
+export default function handler(req, res) {
+  const body = JSON.parse(req.body);
+  console.log(body);
+  res.status(200).json({ status: 'Ok' })
 }
-
